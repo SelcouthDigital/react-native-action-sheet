33721aa2318c85ab9ffb72cae30ba7a4b77e63a2
Simon Wierzchowski <simon.w@selcouth.digital>
2019-05-05 21:00:14 +0100

    fix(App): instances removed

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,11 @@
-/**
- * @format
- */
+import 'react-native'
+import React from 'react'
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import Simple from '../examples/Simple';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(<Simple />);
 });
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native'
-import App from './App'
+
 import { name as appName } from './app.json'
 
 import Simple from './examples/Simple'
